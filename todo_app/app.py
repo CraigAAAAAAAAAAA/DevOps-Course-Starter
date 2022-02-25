d9e0ebc39da890fe1ce164c9d392f2690560e6b2
CraigAAAAAAAAAAA <craig.beasley@lv.co.uk>
2022-02-25 12:42:42 +0000

    Start class

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from todo_app.data.session_items import get_items, add_item
+from todo_app.item import Item
 
 from todo_app.flask_config import Config
 import requests
