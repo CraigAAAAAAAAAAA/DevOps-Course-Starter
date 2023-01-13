cbc494f8795e7fb3ac40d1ff75c0ee15ffcfb27d
Craig Beasley <craigbeasley1981@hotmail.co.uk>
2023-01-13 09:55:07 +0000

    update to flask_config

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -8,4 +8,4 @@ class Config:
         if not self.SECRET_KEY:
             raise ValueError("No SECRET_KEY set for Flask application. Did you follow the setup instructions?")
         self.LOGGLY_TOKEN = os.environ.get('LOGGLY_TOKEN')
-        self.LOG_LEVEL = os.environ.get('LOG_LEVEL')
+        self.LOG_LEVEL = os.environ.get('LOG_LEVEL' 'INFO')
